0a610771ca6cc3a0671e26ad3950dc562dc7e0cd
yufeng218 <1158690013@qq.com>
2019-05-26 23:28:40 +0800

    服务的发现

--- microservicecloud-provider-dept-8001/src/main/java/com/yufeng/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/yufeng/springcloud/DeptProvider8001_App.java
@@ -2,13 +2,15 @@ package com.yufeng.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by Administrator on 2019/4/8.
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaClient     //本服务启动后自动注册到eureka中
+@EnableDiscoveryClient  //服务的发现, 暴露出来
 public class DeptProvider8001_App
 {
     public static void main(String[] args)
