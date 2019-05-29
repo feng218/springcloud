dce76932d3102f73fedf7fc85b192d577bd8eb9f
yufeng218 <1158690013@qq.com>
2019-05-29 23:11:26 +0800

    Ribbon的负载均衡搭建

---
+++ microservicecloud-provider-dept-8003/src/main/java/com/yufeng/springcloud/DeptProvider8003_App.java
@@ -0,0 +1,20 @@
+package com.yufeng.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created by Administrator on 2019/4/8.
+ */
+@SpringBootApplication
+@EnableEurekaClient     //本服务启动后自动注册到eureka中
+@EnableDiscoveryClient  //服务的发现, 暴露出来
+public class DeptProvider8003_App
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DeptProvider8003_App.class, args);
+    }
+}
---
+++ microservicecloud-provider-dept-8003/src/main/java/com/yufeng/springcloud/controller/DeptController.java
@@ -0,0 +1,56 @@
+package com.yufeng.springcloud.controller;
+
+import com.yufeng.springcloud.entities.Dept;
+import com.yufeng.springcloud.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+public class DeptController
+{
+	@Autowired
+    //@Resource(name = "deptServiceImpl")
+	private DeptService service;
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@RequestMapping(value = "/dept/add", method = RequestMethod.POST)
+	public boolean add(@RequestBody Dept dept)
+	{
+		return service.add(dept);
+	}
+
+	@RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
+	public Dept get(@PathVariable("id") Long id)
+	{
+		return service.get(id);
+	}
+
+	@RequestMapping(value = "/dept/get/list", method = RequestMethod.GET)
+	public List<Dept> list()
+	{
+		return service.list();
+	}
+
+	@RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+	public Object discovery()
+	{
+		List<String> list = discoveryClient.getServices(); //发现eureka中的微服务列表
+		System.out.println("eureka中所有的微服务的name列表" + list);
+
+		//从eureka中获取指定name的微服务的信息(yml中配置的spring.application.name)
+		List<ServiceInstance> instances = discoveryClient.getInstances(list.get(0));
+		for(ServiceInstance instance : instances)
+		{
+			System.out.println(instance.getServiceId() + "\t" + instance.getHost() + "\t"
+			+ instance.getPort() + "\t" + instance.getUri());
+		}
+		return this.discoveryClient;
+	}
+}
