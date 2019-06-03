6fa33086eb6bea2a225970221c7cc0bb05703f8d
yufeng218 <1158690013@qq.com>
2019-06-03 23:40:57 +0800

    负载均衡的随机算法的配置

--- microservicecloud-consumer-dept-80/src/main/java/com/yufeng/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/yufeng/springcloud/cfgbeans/ConfigBean.java
@@ -1,5 +1,7 @@
 package com.yufeng.springcloud.cfgbeans;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,5 +16,11 @@ public class ConfigBean
 	public RestTemplate getRestTemplate()
 	{
 		return new RestTemplate();
+	}
+
+	@Bean
+	public IRule myRule()
+	{
+		return new RandomRule();
 	}
 }
