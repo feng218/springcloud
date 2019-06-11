429a4ada5490189f13d27187fa7d21f55c379f6e
yufeng218 <1158690013@qq.com>
2019-06-11 23:05:47 +0800

    feign的初步使用

---
+++ microservicecloud-consumer-dept-feign/src/main/java/com/yufeng/springcloud/controller/DeptController.java
@@ -0,0 +1,34 @@
+package com.yufeng.springcloud.controller;
+
+import com.yufeng.springcloud.entities.Dept;
+import com.yufeng.springcloud.service.FeignConsumerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2019/6/11.
+ */
+@RestController
+public class DeptController
+{
+    @Autowired
+    private FeignConsumerService feignConsumerService;
+
+    @RequestMapping(value = "/consumer/dept/get/{id}", method = RequestMethod.GET)
+    public Dept get(@PathVariable("id") Long id)
+    {
+        return feignConsumerService.get(id);
+    }
+
+    @RequestMapping(value = "/consumer/dept/get/list", method = RequestMethod.GET)
+    public List<Dept> list()
+    {
+        return feignConsumerService.list();
+    }
+
+}
---
+++ microservicecloud-consumer-dept-feign/src/main/java/com/yufeng/springcloud/service/FeignConsumerService.java
@@ -0,0 +1,26 @@
+package com.yufeng.springcloud.service;
+
+import com.yufeng.springcloud.entities.Dept;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2019/6/11.
+ */
+@FeignClient(value = "microservicecloud-dept")
+public interface FeignConsumerService
+{
+    @RequestMapping(value = "/dept/add", method = RequestMethod.POST)
+    public boolean add(@RequestBody Dept dept);
+
+    @RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
+    public Dept get(@PathVariable("id") Long id);
+
+    @RequestMapping(value = "/dept/get/list", method = RequestMethod.GET)
+    public List<Dept> list();
+}
