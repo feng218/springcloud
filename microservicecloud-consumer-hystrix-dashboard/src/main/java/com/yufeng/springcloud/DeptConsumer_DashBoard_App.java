934871135a756a22817f6e9de89674ca6336990d
yufeng218 <1158690013@qq.com>
2019-09-08 23:19:21 +0800

    Hystrix-DashBoard

---
+++ microservicecloud-consumer-hystrix-dashboard/src/main/java/com/yufeng/springcloud/DeptConsumer_DashBoard_App.java
@@ -0,0 +1,18 @@
+package com.yufeng.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Created by Administrator on 2019/4/8.
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class DeptConsumer_DashBoard_App
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DeptConsumer_DashBoard_App .class, args);
+    }
+}
