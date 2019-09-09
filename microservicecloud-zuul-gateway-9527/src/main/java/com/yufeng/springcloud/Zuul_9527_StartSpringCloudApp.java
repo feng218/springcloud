dc9877ece810c0dab0b5973d7bbc8a8cf40ea682
yufeng218 <1158690013@qq.com>
2019-09-09 23:32:17 +0800

    zuul的服务路由映射

---
+++ microservicecloud-zuul-gateway-9527/src/main/java/com/yufeng/springcloud/Zuul_9527_StartSpringCloudApp.java
@@ -0,0 +1,18 @@
+package com.yufeng.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by Administrator on 2019/9/9.
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class Zuul_9527_StartSpringCloudApp
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Zuul_9527_StartSpringCloudApp.class);
+    }
+}
